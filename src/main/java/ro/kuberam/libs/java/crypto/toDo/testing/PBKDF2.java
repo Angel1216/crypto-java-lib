32fb3987178992f348b2b2d02927efccc6961ac5
angelmlnova <amartinez@novasolutionsystems.com>
2017-07-20 22:44:03 -0500

    nueva clase y dependencia

---
+++ src/main/java/ro/kuberam/libs/java/crypto/toDo/testing/PBKDF2.java
@@ -0,0 +1,27 @@
+package ro.kuberam.libs.java.crypto.toDo.testing;
+
+import java.nio.charset.Charset;
+
+import org.bouncycastle.util.encoders.Base64;
+
+import ro.kuberam.libs.java.crypto.toDo.*;
+
+public class PBKDF2 {
+
+    public static void main(String[] args) {
+        try {
+            String password = "iamtwentycharacterss";
+            String salt = "50.eGIYr3ZpxpWw67utH17s/A==";
+            int iterations = Integer.parseInt(salt.substring(0, salt.indexOf('.')));
+            byte[] saltBytes = salt.getBytes(Charset.forName("UTF-8"));
+
+            Rfc2898DeriveBytes rfc2898 = new Rfc2898DeriveBytes(password, saltBytes, iterations);
+            byte[] key = rfc2898.getBytes(64);
+            String hash = new String(Base64.encode(key));
+            System.out.println(hash);
+        } catch (Exception ex) {
+            System.out.println("ERROR: " + ex);
+        }
+    }
+
+}
